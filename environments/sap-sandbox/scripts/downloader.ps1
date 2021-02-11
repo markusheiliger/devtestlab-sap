e3d7585ba9378e6254af60a2e8b569da464148c7
Markus Heiliger <markus.heiliger@microsoft.com>
2021-02-11 09:04:29 -0800

    ...

--- environments/sap-sandbox/scripts/downloader.ps1
+++ environments/sap-sandbox/scripts/downloader.ps1
@@ -14,52 +14,57 @@ param (
 )
 
 foreach ($Package in $Packages) {
+	try {
+		$DownloadRoot = Join-Path $PSScriptRoot "downloads/$_"
+		Write-Output "Processing package '$Package' ($DownloadRoot) ..."
 
-	$DownloadRoot = Join-Path $PSScriptRoot "downloads/$_"
-	Write-Output "Processing package '$Package' ($DownloadRoot) ..."
+		if ($Refresh) {
+			# delete package download location to enforce download
+			Remove-Item -Confirm:$false -Recurse -Force -Path $DownloadRoot | Out-Null
+		}
 
-	if ($Refresh) {
-		# delete package download location to enforce download
-		Remove-Item -Confirm:$false -Recurse -Force -Path $DownloadRoot | Out-Null
-	}
+		if ( -not (Test-Path $DownloadRoot -PathType Container)) {
+			# create download folder for the selected package
+			New-Item -ItemType Directory -Force -Path $DownloadRoot | Out-Null
+		}
 
-	if ( -not (Test-Path $DownloadRoot -PathType Container)) {
-		# create download folder for the selected package
-		New-Item -ItemType Directory -Force -Path $DownloadRoot | Out-Null
-	}
+		$PackageFiles = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lnwsoft/phoenix-repo-downloader/main/packages/$PackageName.lst").Content | `
+			Where-Object { [system.uri]::IsWellFormedUriString($_, [System.UriKind]::Absolute) } | `
+			Select-Object @{label = "ID"; expression = { $_.ToString().Split("/") | Select-Object -Last 1 } }, @{label = "Url"; expression = { $_.ToString() } } -Unique
+		$PackageFiles | Format-Table
 
-	$PackageFiles = (Invoke-WebRequest -Uri "https://raw.githubusercontent.com/lnwsoft/phoenix-repo-downloader/main/packages/$PackageName.lst").Content | `
-		Where-Object { [system.uri]::IsWellFormedUriString($_, [System.UriKind]::Absolute) } | `
-		Select-Object @{label = "ID"; expression = { $_.ToString().Split("/") | Select-Object -Last 1 } }, @{label = "Url"; expression = { $_.ToString() } } -Unique
-	$PackageFiles | Format-Table
+		$PasswordSec = ConvertTo-SecureString $Password -AsPlainText -Force
+		$Credentials = New-Object System.Management.Automation.PSCredential($Username, $PasswordSec)
 
-	$PasswordSec = ConvertTo-SecureString $Password -AsPlainText -Force
-	$Credentials = New-Object System.Management.Automation.PSCredential($Username, $PasswordSec)
+		$PackageFiles | ForEach-Object {
 
-	$PackageFiles | ForEach-Object {
+			Write-Output "- Enqueued download '$($_.Url)'"
 
-		Write-Output "- Enqueued download '$($_.Url)'"
+			$Download = Invoke-WebRequest -Uri "https://origin.softwaredownloads.sap.com/tokengen/" `
+				-Credential $Credentials -UserAgent "SAP Download Manager"  -Method Get `
+				-Body @{ file = $_.Id } # -OutFile $DownloadPath
 
-		$Download = Invoke-WebRequest -Uri "https://origin.softwaredownloads.sap.com/tokengen/" `
-			-Credential $Credentials -UserAgent "SAP Download Manager"  -Method Get `
-			-Body @{ file = $_.Id } # -OutFile $DownloadPath
-
-		if ($Download.Headers["Content-Disposition"] -match 'filename=\"(.+)\"') {
+			if ($Download.Headers["Content-Disposition"] -match 'filename=\"(.+)\"') {
 		
-			[string] $DownloadFile = $matches[1].ToString()
+				[string] $DownloadFile = $matches[1].ToString()
 
-			if ($Package.ToLowerInvariant() = "hostagent") {
-				# the hostagent package needs some file name cleanup to take place
-				$DownloadFile = $DownloadFile -replace "(?<=SAPCAR|SAPHOSTAGENT)(.*)(?=\.)", ""
+				if ($Package.ToLowerInvariant() = "hostagent") {
+					# the hostagent package needs some file name cleanup to take place
+					$DownloadFile = $DownloadFile -replace "(?<=SAPCAR|SAPHOSTAGENT)(.*)(?=\.)", ""
+				}
+
+				[string] $DownloadPath = Join-Path $DownloadRoot $DownloadFile
+				[IO.File]::WriteAllBytes($DownloadPath, $Download.Content)
+
+				Write-Output "- Saved download '$($_.Url)' to '$DownloadPath'"
 			}
-
-			[string] $DownloadPath = Join-Path $DownloadRoot $DownloadFile
-			[IO.File]::WriteAllBytes($DownloadPath, $Download.Content)
-
-			Write-Output "- Saved download '$($_.Url)' to '$DownloadPath'"
+			else {
+				throw "Unable to identify file name in download $($p.Url)"
+			}
 		}
-		else {
-			throw "Unable to identify file name in download $($p.Url)"
-		}
+	}
+	catch {
+
+		Write-Output "ERROR: $($_.Exception.Message)" 
 	}
 }
