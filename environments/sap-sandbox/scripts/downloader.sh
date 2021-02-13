b2ea826aa3cdf4fdca9be3bfe02c5d9ce7bab9ab
Markus Heiliger <markus.heiliger@microsoft.com>
2021-02-12 21:57:10 -0800

    ...

--- environments/sap-sandbox/scripts/downloader.sh
+++ environments/sap-sandbox/scripts/downloader.sh
@@ -18,61 +18,49 @@ fail () {
 
 [ -z "$PARAM_SAPUSERNAME" ] && { PARAM_SAPUSERNAME="$SAPUsername"; } 
 [ -z "$PARAM_SAPUSERNAME" ] && fail "Parameter --SAPUsername is mandatory"
-
 [ -z "$PARAM_SAPPASSWORD" ] && { PARAM_SAPPASSWORD="$SAPPassword"; } 
 [ -z "$PARAM_SAPPASSWORD" ] && fail "Parameter --SAPPassword is mandatory"
+[ -z "$PARAM_PACKAGE" ] && fail "Parameter --Package is mandatory"
 
-[ -z "$PARAM_PACKAGES" ] && fail "Parameter --Packages is mandatory"
+readonly DOWNLOAD_ROOT="$DIR/../download" # re-create download root and switch context
+rm -rf $DOWNLOAD_ROOT && mkdir -p "$DOWNLOAD_ROOT" && pushd "$DOWNLOAD_ROOT" > /dev/null
 
-while read TOKEN; do
-	[ -z "$TOKEN" ] || { PACKAGES+=( "$TOKEN" ); }
-done < <( echo "$PARAM_PACKAGES" | tr ";" "\n" | tr "," "\n" )
-
-readonly DOWNLOAD_ROOT="$DIR/../downloads"
-rm -rf $DOWNLOAD_ROOT && mkdir -p "$DOWNLOAD_ROOT"
-
-# tee output to downloader log (our own littel log)
-exec &> >(tee -a "$DOWNLOAD_ROOT/downloader.log")
+# tee output to downloader log 
+exec &> >(tee -a downloader.log)
 
 # install a full featured wget
 echo -e "\nUpgrading wget ...\n"
 apk update && apk add wget
 
-for PACKAGE in "${PACKAGES[@]}"; do
+while read LINE; do
+	[[ "$LINE" = "https://softwaredownloads.sap.com/file/"* ]] && { URLS+=( "$LINE" ); }
+done < <( curl -s "https://raw.githubusercontent.com/lnwsoft/phoenix-repo-downloader/main/packages/$PARAM_PACKAGE.lst" )
+
+for URL in "${URLS[@]}"; do
+
+	echo -e "\nDownloading file $URL into $PWD ...\n"
 	
-	mkdir -p "$DOWNLOAD_ROOT/$PACKAGE" && pushd "$DOWNLOAD_ROOT/$PACKAGE" > /dev/null
-
-	while read LINE; do
-		[[ "$LINE" = "https://softwaredownloads.sap.com/file/"* ]] && { URLS+=( "$LINE" ); }
-	done < <( curl -s "https://raw.githubusercontent.com/lnwsoft/phoenix-repo-downloader/main/packages/$PACKAGE.lst" )
-
-	for URL in "${URLS[@]}"; do
-
-		echo -e "\nDownloading file $URL into $PWD ...\n"
-		
-		wget --user="$PARAM_SAPUSERNAME" --password="$PARAM_SAPPASSWORD" \
-			--content-disposition --trust-server-names --auth-no-challenge \
-			--no-verbose --user-agent="SAP Download Manager" "$URL"
-		
-	done
-
-	if [ "${PACKAGE^^}" = "HOSTAGENT" ]; then
-
-		echo -e "\nRenaming hostagent packages ...\n"
-		mv SAPCAR*.EXE SAPCAR.EXE > /dev/null 
-		mv SAPHOSTAGENT*.SAR SAPHOSTAGENT.SAR > /dev/null
-
-	fi
-
-	if [ ! -z "$StorageName" ] && [ ! -z "$StorageKey" ]; then
-
-		echo -e "\nUploading packages ...\n"
-		az storage blob upload-batch \
-			--account-name "$StorageName" --account-key "$StorageKey" \
-			--destination "$PACKAGE" --source "$PWD" --no-progress -o none
-
-	fi
-
-	popd > /dev/null
-
+	wget --user="$PARAM_SAPUSERNAME" --password="$PARAM_SAPPASSWORD" \
+		--content-disposition --trust-server-names --auth-no-challenge \
+		--no-verbose --user-agent="SAP Download Manager" "$URL"
+	
 done
+
+if [ "${PARAM_PACKAGE^^}" = "HOSTAGENT" ]; then
+
+	echo -e "\nRenaming hostagent packages ...\n"
+	mv SAPCAR*.EXE SAPCAR.EXE > /dev/null 
+	mv SAPHOSTAGENT*.SAR SAPHOSTAGENT.SAR > /dev/null
+
+fi
+
+if [ ! -z "$StorageName" ] && [ ! -z "$StorageKey" ]; then
+
+	echo -e "\nUploading packages ...\n"
+	az storage blob upload-batch \
+		--account-name "$StorageName" --account-key "$StorageKey" \
+		--destination "$PARAM_PACKAGE" --source "$PWD" --no-progress -o none
+
+fi
+
+popd > /dev/null
